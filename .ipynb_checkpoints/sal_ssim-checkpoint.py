e91666a691563c187d36463c142d4e3808eb73e7
Alexkkir <alexkkir@gmail.com>
2022-10-17 22:31:08 +0300

    some changes

---
+++ .ipynb_checkpoints/sal_ssim-checkpoint.py
@@ -0,0 +1,111 @@
+from typing import Tuple, List, Optional, Union, Dict, Any
+import torch
+import torch.nn.functional as F
+from torch.nn.modules.loss import _Loss
+from piq.utils import _validate_input, _reduce
+from piq.functional import gaussian_filter
+
+
+def ssim(x: torch.Tensor, y: torch.Tensor, mask, kernel_size: int = 11, kernel_sigma: float = 1.5,
+         data_range: Union[int, float] = 1., reduction: str = 'mean',
+         downsample: bool = True, k1: float = 0.01, k2: float = 0.03) -> torch.Tensor:
+    assert kernel_size % 2 == 1, f'Kernel size must be odd, got [{kernel_size}]'
+    _validate_input([x, y], dim_range=(4, 5), data_range=(0, data_range))
+
+    x = x / float(data_range)
+    y = y / float(data_range)
+
+    # Averagepool image if the size is large enough
+    f = max(1, round(min(x.size()[-2:]) / 256))
+    if (f > 1) and downsample:
+        x = F.avg_pool2d(x, kernel_size=f)
+        y = F.avg_pool2d(y, kernel_size=f)
+        mask = F.avg_pool2d(mask, kernel_size=f)
+
+    kernel = gaussian_filter(kernel_size, kernel_sigma).repeat(x.size(1), 1, 1, 1).to(y)
+    ssim_map = _ssim_per_channel(x=x, y=y, mask=mask, kernel=kernel, data_range=data_range, k1=k1, k2=k2)
+    ssim_val = ssim_map.mean(1)
+
+    ssim_val = _reduce(ssim_val, reduction)
+    return ssim_val
+
+
+def _ssim_per_channel(x: torch.Tensor, y: torch.Tensor, mask, kernel: torch.Tensor,
+                      data_range: Union[float, int] = 1., k1: float = 0.01,
+                      k2: float = 0.03) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+    if x.size(-1) < kernel.size(-1) or x.size(-2) < kernel.size(-2):
+        raise ValueError(f'Kernel size can\'t be greater than actual input size. '
+                         f'Input size: {x.size()}. Kernel size: {kernel.size()}')
+
+    c1 = k1 ** 2
+    c2 = k2 ** 2
+    n_channels = x.size(1)
+    mu_x = F.conv2d(x, weight=kernel, stride=1, padding=0, groups=n_channels)
+    mu_y = F.conv2d(y, weight=kernel, stride=1, padding=0, groups=n_channels)
+
+    mu_xx = mu_x ** 2
+    mu_yy = mu_y ** 2
+    mu_xy = mu_x * mu_y
+
+    sigma_xx = F.conv2d(x ** 2, weight=kernel, stride=1, padding=0, groups=n_channels) - mu_xx
+    sigma_yy = F.conv2d(y ** 2, weight=kernel, stride=1, padding=0, groups=n_channels) - mu_yy
+    sigma_xy = F.conv2d(x * y, weight=kernel, stride=1, padding=0, groups=n_channels) - mu_xy
+
+    mask = F.conv2d(mask, weight=kernel, stride=1, padding=0, groups=n_channels)
+
+    # Contrast sensitivity (CS) with alpha = beta = gamma = 1.
+    cs = (2. * sigma_xy + c2) / (sigma_xx + sigma_yy + c2)
+
+    # Structural similarity (SSIM)
+    ss = (2. * mu_xy + c1) / (mu_xx + mu_yy + c1) * cs
+
+    ss = (ss * mask) / mask.mean()
+
+    ssim_val = ss.mean(dim=(-1, -2))
+    return ssim_val
+
+
+def _reduce(x: torch.Tensor, reduction: str = 'mean') -> torch.Tensor:
+    if reduction == 'none':
+        return x
+    elif reduction == 'mean':
+        return x.mean(dim=0)
+    elif reduction == 'sum':
+        return x.sum(dim=0)
+    else:
+        raise ValueError("Unknown reduction. Expected one of {'none', 'mean', 'sum'}")
+
+
+def _validate_input(
+        tensors: List[torch.Tensor],
+        dim_range: Tuple[int, int] = (0, -1),
+        data_range: Tuple[float, float] = (0., -1.),
+        # size_dim_range: Tuple[float, float] = (0., -1.),
+        size_range: Optional[Tuple[int, int]] = None,
+) -> None:
+    if not __debug__:
+        return
+
+    x = tensors[0]
+
+    for t in tensors:
+        assert torch.is_tensor(t), f'Expected torch.Tensor, got {type(t)}'
+        assert t.device == x.device, f'Expected tensors to be on {x.device}, got {t.device}'
+
+        if size_range is None:
+            assert t.size() == x.size(), f'Expected tensors with same size, got {t.size()} and {x.size()}'
+        else:
+            assert t.size()[size_range[0]: size_range[1]] == x.size()[size_range[0]: size_range[1]], \
+                f'Expected tensors with same size at given dimensions, got {t.size()} and {x.size()}'
+
+        if dim_range[0] == dim_range[1]:
+            assert t.dim() == dim_range[0], f'Expected number of dimensions to be {dim_range[0]}, got {t.dim()}'
+        elif dim_range[0] < dim_range[1]:
+            assert dim_range[0] <= t.dim() <= dim_range[1], \
+                f'Expected number of dimensions to be between {dim_range[0]} and {dim_range[1]}, got {t.dim()}'
+
+        if data_range[0] < data_range[1]:
+            assert data_range[0] <= t.min(), \
+                f'Expected values to be greater or equal to {data_range[0]}, got {t.min()}'
+            assert t.max() <= data_range[1], \
+                f'Expected values to be lower or equal to {data_range[1]}, got {t.max()}'
---
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,133 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torchvision.transforms as T
+import skvideo.io
+from pathlib import Path
+import os
+import csv
+from time import time
+from threading import BoundedSemaphore, Thread
+import traceback
+import math
+import pathlib
+import json
+from copy import deepcopy
+import pickle as pkl
+import logging
+
+
+
+
+class VideoReader:
+    def __init__(self, path, shape=None, batch_size=10, verbose=False):
+        if str(path).endswith('yuv'):
+            self.vr = skvideo.io.vreader(path, 1080, 1920, inputdict={'-pix_fmt': 'yuvj420p'})
+        else:
+            self.vr = skvideo.io.vreader(path)
+        self.batch_size = batch_size
+        self.shape = shape or tuple(int(x) for x in tuple(torch.Tensor(skvideo.io.FFmpegReader(path).getShape())))
+        self.len = self.shape[0]
+        self.pos = 0
+        self.time_out = 10
+        self.verbose = verbose
+        
+    def __iter__(self):
+        for batch_num in range(0, self.len, self.batch_size):
+            if self.verbose and batch_num % self.time_out == 0:
+                print(f'batch {batch_num // self.batch_size} / {self.len // self.batch_size}')
+                
+            batch_size = min(self.batch_size, self.len - self.pos)
+            shape = (batch_size, *self.shape[1:])
+            
+            batch = np.zeros(shape)
+            
+            for i, frame in enumerate(self.vr):
+                if i < batch_size:
+                    if frame.shape != self.shape[1:]:
+                        frame = cv2.resize(frame, (shape[2], shape[1]))
+                    batch[i, ...] = frame / 255
+                else:
+                    break
+            batch = torch.Tensor(batch).permute(0, 3, 1, 2)
+            yield batch
+            
+class MyWriter:
+    def __init__(self, f):
+        self.filename = f
+        if not os.path.exists(self.filename):
+            os.open(self.filename, os.O_CREAT)
+
+    def write_row(self, row):
+        with open(self.filename, 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(row)
+
+    def write_text(self, text):
+        with open(self.filename, 'a') as f:
+            f.write(text)
+            print(text)
+            
+def folder2name(folder):
+    name = os.path.basename(os.path.normpath(folder))
+    name = "_".join(name.split("_")[:-1])
+    return name
+
+class CommandHandler:
+    def __init__(self, max_processes=2):
+        self.maxp = max_processes
+        self.sem = BoundedSemaphore(self.maxp)
+    def run(self, target, args):
+        def func_with_end(args):
+            target(*args)
+            self.sem.release()
+            # print('end')
+            
+        self.sem.acquire()
+        thread = Thread(target=func_with_end, args=(args,))        
+        thread.start()
+        
+def listdir(dir_name: Path):
+    return sorted([dir_name / file for file in os.listdir(dir_name) if '.ipynb' not in str(file)])
+
+def json_representation_of_dataset(dataset_path):
+    json_data = {}
+    for sequence_folder in os.listdir(dataset_path / 'seq'):
+        sequence_name = sequence_folder.removesuffix('_x265')
+        
+        reference = [str(file) for file in listdir(dataset_path / 'ref') if sequence_name in str(file)][0]
+        mask = [str(file) for file in listdir(dataset_path / 'masks') if sequence_name in str(file)][0]
+        distorted = [str(file) for file in listdir(dataset_path / 'seq' / sequence_folder)]
+        
+        json_data[sequence_name] = {
+            'mask': mask,
+            'ref': reference,
+            'dis': distorted
+        }
+        
+    return json_data
+
+
+def safe_mean(arr):
+    arr = np.array(arr)
+    arr = arr[np.isfinite(arr)]
+    val = float(arr.mean())
+    return val
+
+def get_logger(log_filepath):
+    logger = logging.getLogger('Logger')
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(levelname)-7s || %(asctime)s.%(msecs)03d || %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+
+    fh = logging.FileHandler(log_filepath, 'a+')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+    return logger
